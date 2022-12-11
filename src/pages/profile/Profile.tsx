f9e33d98d7edf5a4877d7da6d8ffa6be4e560f7e
Aliaksei Yausiuk <Aliakseiyausiuk@gmail.com>
2022-12-11 22:58:18 +0300

    fix: add edit name user

--- src/pages/profile/Profile.tsx
+++ src/pages/profile/Profile.tsx
@@ -1,42 +1,73 @@
-import React, {useContext, useEffect} from 'react'
-import {HeaderTitleContext} from '../../context/context'
-import {setPageTitle} from '../../services/pageTitle'
-import {useAppDispatch, useAppSelector} from '../../state/store'
-import {Avatar, Badge, Grid, IconButton, Paper, Typography} from '@mui/material';
+import React, { useContext, useEffect } from 'react'
+import { HeaderTitleContext } from '../../context/context'
+import { setPageTitle } from '../../services/pageTitle'
+import { useAppDispatch, useAppSelector } from '../../state/store'
+import {
+  Avatar,
+  Badge,
+  Box,
+  Card,
+  Grid,
+  IconButton,
+  Typography,
+} from '@mui/material'
 import styles from './profile.module.css'
-import CameraAltIcon from '@mui/icons-material/CameraAlt';
-import XButton from '../../common/components/button/XButton';
-import {logout} from '../../features/auth/authSlice';
-
+import CameraAltIcon from '@mui/icons-material/CameraAlt'
+import XButton from '../../common/components/button/XButton'
+import { logout } from '../../features/auth/authSlice'
+import { EditNameUser } from '../../common/components/editNameUser/EditNameUser'
 
 export const Profile = () => {
-    const dispatch = useAppDispatch();
-    const {setTitle} = useContext(HeaderTitleContext)
-    const user = useAppSelector((state) => state.auth.user)
-    useEffect(() => {
-        setTitle('Profile')
-        setPageTitle('Profile')
-    }, [])
+  const dispatch = useAppDispatch()
+  const { setTitle } = useContext(HeaderTitleContext)
+  const user = useAppSelector((state) => state.auth.user)
+  useEffect(() => {
+    setTitle('Profile')
+    setPageTitle('Profile')
+  }, [])
 
-    const logoutHandler = () => {
-        dispatch(logout)
+  const logoutHandler = () => {
+    dispatch(logout)
+  }
 
-    }
+  const onOpenChange = () => {
+    console.log('dsdsd') // заглушка для prettera нужно за диспачить новое имя в state
+  }
 
-    return (
-        <Grid container justifyContent={'center'} alignItems={'center'}>
-            <Paper className={styles.pageContainer}>
-                <Typography className={styles.title} variant={'h5'}>Personal Information</Typography>
-                <Badge badgeContent={<IconButton><CameraAltIcon/></IconButton>} showZero anchorOrigin={{
-                    vertical: 'bottom',
-                    horizontal: 'right',
-                }}>
-                    <Avatar alt={'Foto user'} src={user.avatar} sx={{width: 96, height: 96}}/>
-                </Badge>
-                <Typography className={styles.name} component={'p'}>{user.name}</Typography>
-                <Typography className={styles.email} component={'p'}>{user.email}</Typography>
-                <XButton className={styles.btn} onClick={logoutHandler}>Log out</XButton>
-            </Paper>
-        </Grid>
-    )
+  return (
+    <Grid container justifyContent={'center'} alignItems={'center'}>
+      <Card className={styles.pageContainer}>
+        <Typography className={styles.title} variant={'h5'}>
+          Personal Information
+        </Typography>
+        <Badge
+          badgeContent={
+            <IconButton>
+              <CameraAltIcon />
+            </IconButton>
+          }
+          showZero
+          anchorOrigin={{
+            vertical: 'bottom',
+            horizontal: 'right',
+          }}
+        >
+          <Avatar
+            alt={'Foto user'}
+            src={user.avatar}
+            sx={{ width: 96, height: 96 }}
+          />
+        </Badge>
+        <Box className={styles.name}>
+          <EditNameUser value={user.name} onChange={onOpenChange} />
+        </Box>
+        <Typography className={styles.email} component={'p'}>
+          {user.email}
+        </Typography>
+        <XButton className={styles.btn} onClick={logoutHandler}>
+          Log out
+        </XButton>
+      </Card>
+    </Grid>
+  )
 }
