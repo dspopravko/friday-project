d774f58c8bdaff4f926b4bee425588da2acfc150
dspopravko <demid1498@gmail.com>
2022-12-11 15:37:24 +0400

    code formatter: eslint

---
+++ .eslintrc.js
@@ -0,0 +1,26 @@
+module.exports = {
+  env: {
+    node: true,
+    es2021: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
+  ],
+  overrides: [],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['react', '@typescript-eslint'],
+  rules: {
+    'no-nested-ternary': 'error',
+    'no-duplicate-imports': 'error',
+    camelcase: 'warn',
+    curly: 'error',
+    eqeqeq: 'warn',
+    'max-depth': ['warn', 3],
+  },
+}
