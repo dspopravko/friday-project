b0d726e77b7aa167c31a77e187dd5e92cf6e9697
dspopravko <demid1498@gmail.com>
2022-12-15 20:51:30 +0400

    import fix

--- src/common/succesSignup/SuccessSignup.tsx
+++ src/common/succesSignup/SuccessSignup.tsx
@@ -1,20 +1,19 @@
 import React from 'react'
 import { Alert, Snackbar } from '@mui/material'
-import { Navigate, redirect } from 'react-router-dom'
+import { redirect } from 'react-router-dom'
 import { PATH } from '../../data/paths'
-import { regAction } from '../../features/auth/components/signUp/form/registrationSlice'
+import { setReg } from '../../features/auth/components/signUp/form/signUpSlice'
 import { useAppDispatch, useAppSelector } from '../../state/store'
 
 export const SuccessSignup = () => {
   const success = useAppSelector((state) => state.reg.registered)
   const dispatch = useAppDispatch()
-  //   const path = '/' + PATH.LOGIN.MAIN
   const handleClose = () => {
-    dispatch(regAction({ registered: false }))
+    dispatch(setReg(false))
     return redirect(PATH.LOGIN.MAIN)
   }
   return (
-    <Snackbar open={!!success} autoHideDuration={2000} onClose={handleClose}>
+    <Snackbar open={success} autoHideDuration={2000} onClose={handleClose}>
       <Alert onClose={handleClose} severity="success" sx={{ width: '100%' }}>
         Registration success!
       </Alert>
